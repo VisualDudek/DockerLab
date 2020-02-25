a63bb93c8823ccf337af2534c57746605f4d9b0c
Visual <dudek.v@gmail.com>
2020-02-25 15:02:23 +0100

    Add grafana and cAdvisor

---
+++ start_grafana.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker run \
+	-d \
+	--name=grafana \
+	-p 3000:3000 \
+	grafana/grafana
--- start_prometheus.sh
+++ start_prometheus.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker run \
+	-d \
         -p 9090:9090 \
 	-v $(pwd)/prometheus_docker.yml:/etc/prometheus/prometheus.yml \
         prom/prometheus
